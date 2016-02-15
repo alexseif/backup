108f229ab0b622332ee61e8a60bd3c639dec558a
Alex Seif <alexseif@users.noreply.github.com>
2016-02-15 09:41:41 +0200

    Enhancing script
    Adding more control over configurations
    Adding SCP support
    Backup mysql by db not db/table
    Organize backups by date

--- backup.sh
+++ backup.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
-# What folders should be backupped?
-declare -a what_to_backup=('/var/www/');
+#########################
+######TO BE MODIFIED#####
+
+# What folders should be backup'd?
+declare -a what_to_backup=('path1' 'path2');
 
 # Path where the backup and the backup folders should be stored
 BACKUP_PATH="/root/backup/"
 # Name of the folders to store the backups in
-SQL_FOLDERNAME="sqldata"
-DATA_FOLDERNAME="webdata"
+SQL_FOLDERNAME="db"
+DATA_FOLDERNAME=""
 
 BACKUP_MYSQL="1"
 # MySql login information
 MYSQL_USER=""
 MYSQL_PASSWORD=""
 
+### FTP server Setup ###
+SEND_FTP="1"
+FTPD="FTP FOLDER"
+FTPU="FTP USERNAME"
+FTPP="FTP PASSWORD"
+FTPS="FTP HOST"
 
+######DO NOT MAKE MODIFICATION BELOW#####
+#########################################
 
-NOW=$(date +"%d-%m-%Y")
+### Binaries ###
+TAR="$(which tar)"
+GZIP="$(which gzip)"
+SFTP="$(which sftp)"
+SCP="$(which scp)"
+MYSQL="$(which mysql)"
+MYSQLDUMP="$(which mysqldump)"
+
+# File backup name segments
+HOSTNAME=$(hostname -s)
+NOW=$(date +"%Y-%m-%d")
+
+# TODO: implement verbosity
+# override & verbosity
 for flag in "$@"
 do
 	if [ "$flag" == "-o" ]; then
@@ -27,57 +51,63 @@ do
 		FLAG_VERBOSE="1"
 	fi
 done
-
+# Check freediskspace
 FREE_DISCSPACE=`df / -h | awk '{ print $4 }' | tail -n 1 | cut -d "G" -f1`
 
-# If discspace is lower than 15 GB, abort
-if [[ $FREE_DISCSPACE -lt 15 ]]; then
+# If discspace is lower than 1 GB, abort
+if [[ $FREE_DISCSPACE -lt 1 ]]; then
 	echo "Discspace is low! ( Less than 15 GB free. ) Backup aborted."
 	exit
 fi
-
-if [ ! -d "$BACKUP_PATH/$SQL_FOLDERNAME" ]; then
-	mkdir $BACKUP_PATH/$SQL_FOLDERNAME > /dev/null
+# Create directory structure
+if [ ! -d "$BACKUP_PATH/$NOW" ]; then
+	mkdir $BACKUP_PATH/$NOW > /dev/null
 	if [ $? -gt 0 ]; then
-	    echo "Error while creating folder $BACKUP_PATH/$SQL_FOLDERNAME. Aborting."
+	    echo "Error while creating folder $BACKUP_PATH/$NOW. Aborting."
 	    exit
 	fi
 fi
 
-if [ ! -d "$BACKUP_PATH/$DATA_FOLDERNAME" ]; then
-	mkdir $BACKUP_PATH/$DATA_FOLDERNAME
+if [ ! -d "$BACKUP_PATH/$NOW/$SQL_FOLDERNAME" ]; then
+	mkdir $BACKUP_PATH/$NOW/$SQL_FOLDERNAME > /dev/null
 	if [ $? -gt 0 ]; then
-	    echo "Error while creating folder $BACKUP_PATH/$DATA_FOLDERNAME. Aborting."
+	    echo "Error while creating folder $BACKUP_PATH/$NOW/$SQL_FOLDERNAME. Aborting."
 	    exit
 	fi
 fi
 
-cd $BACKUP_PATH/$SQL_FOLDERNAME/
+if [ ! -d "$BACKUP_PATH/$NOW/$DATA_FOLDERNAME" ]; then
+	mkdir $BACKUP_PATH/$NOW/$DATA_FOLDERNAME
+	if [ $? -gt 0 ]; then
+	    echo "Error while creating folder $BACKUP_PATH/$NOW/$DATA_FOLDERNAME. Aborting."
+	    exit
+	fi
+fi
+
+# Backup mysql
+cd $BACKUP_PATH/$NOW/$SQL_FOLDERNAME/
 
 if [ "$BACKUP_MYSQL" == "1" ]; then
 	if [ ! -f ./database_backup.$NOW.tgz ] || [ "$FLAG_OVERWRITE" == "1" ]; then
-		echo "Dumping all MySql-Databases..."
-		mysqldump -u$MYSQL_USER -p$MYSQL_PASSWORD --opt --all-databases > ./databases.sql
-		echo "Done. Packing..."
-		tar -zcvf database_backup.$NOW.tgz *.sql 
-		echo "Done. Removing unpacked dump..."
-		rm -f *.sql
-		echo "Done. MySql-Backup finished into database_backup.$NOW.tgz ."
+		### Get all databases name ###
+		DBS="$($MYSQL -u $MYSQL_USER -h $MYSQL_HOST -p$MYSQL_PASSWORD -Bse 'show databases')"
+		for db in $DBS
+		do
+			### Backup dbs in individual files ###
+			FILE=$BACKUP_PATH/$NOW/$SQL_FOLDERNAME/$db-$NOW.sql.gz
+			echo $db; $MYSQLDUMP --opt --add-drop-table --allow-keywords -q -c -u $MYSQL_USER -h $MYSQL_HOST -p$MYSQL_PASSWORD $db | $GZIP -9 > $FILE
+		done
 	else
 		echo "Database backup for date $NOW already exists, skipping."
 	fi
 fi
 
-
-cd $BACKUP_PATH/$DATA_FOLDERNAME/
+# Backup files
+cd $BACKUP_PATH/$NOW/$DATA_FOLDERNAME/
 
 DIRECTORY=$NOW
 
-if [ ! -d "$DIRECTORY" ]; then
-	mkdir $DIRECTORY
-fi
-
-cd ./$DIRECTORY/
+cd $BACKUP_PATH/$NOW/
 
 for i in "${what_to_backup[@]}"
 do	
@@ -103,3 +133,19 @@ do
 		echo "Backup location $i wasn't found. Skipping."
 	fi
 done
+
+### Compress all backups in one nice file to upload ###
+ARCHIVE=$BACKUP_PATH/$HOSTNAME-$NOW.tar.gz
+ARCHIVED=$NOW
+cd $BACKUP_PATH/
+
+$TAR -cvf $ARCHIVE $ARCHIVED
+
+if [ "$SEND_FTP" == "1" ]; then
+	### Dump backups using FTP ###
+	cd $BACKUP_PATH
+	DUMPFILE=$HOSTNAME-$NOW.tar.gz
+	$SCP $DUMPFILE $FTPU@$FTPS:$FTPD/
+fi
+### Delete the backup dir and keep archive ###
+rm -rf $ARCHIVED
